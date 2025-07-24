bd8582fdd9afcb31b1b22b696794453cbb951409
luiz.nazario <luiz.nazario@agpr5.com>
2025-07-24 16:25:37 -0300

    Atualiza script de desenvolvimento e adiciona rota para listagem de usuários

--- src/server.js
+++ src/server.js
@@ -1,6 +1,12 @@
 import http from "node:http";
 
 const server = http.createServer((req, res) => {
+  const { method, url } = req
+
+  if (method === 'GET' && url === '/users'){
+    return res.end('Listagem')
+  }
+
   return res.end("Hello World");
 });
 
