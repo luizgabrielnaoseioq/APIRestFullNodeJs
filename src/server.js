e711cb973bcc6fb8680faeeabcef0ce200b72965
luiz.nazario <luiz.nazario@agpr5.com>
2025-07-24 17:15:19 -0300

    Implementa listagem e criação de usuários na rota POST /users

--- src/server.js
+++ src/server.js
@@ -1,17 +1,28 @@
+import { log } from "node:console";
 import http from "node:http";
 
+const users = [];
+
 const server = http.createServer((req, res) => {
-  const { method, url } = req
+  const { method, url } = req;
 
-  if (method === 'GET' && url === '/users'){
-    return res.end('Listagem')
+  if (method === "GET" && url === "/users") {
+    return res
+      .setHeader("Content-type", "application/json")
+      .end(JSON.stringify(users));
   }
 
-  if (method === 'POST' && url == '/users'){
-    return res.end('Criando um novo usuario')
+  if (method === "POST" && url === "/users") {
+    users.push({
+      id: 1,
+      name: "John Doe",
+      email: "johndoe@example.com",
+    });
+
+    return res.writeHead(201).end("Criação de usuario");
   }
 
-  return res.end("Hello World");
+  return res.writeHead(404).end("Not found");
 });
 
 server.listen(3333);
