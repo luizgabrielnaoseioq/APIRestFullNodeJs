aacae81a3dd67f03953c0a5a37b87b41d24d48b3
Luiz Gabriel <gabrielon6689@gmail.com>
2025-07-24 22:23:58 -0300

    Add Fastfy in project

--- streams/fundamentals.js
+++ streams/fundamentals.js
@@ -1,6 +1,25 @@
-import { Readable } from "node:stream";
+import { Readable, Writable } from "node:stream";
+
 class OneToHundredStream extends Readable {
   index = 1;
 
-  _read() {}
+  _read() {
+    const i = this.index++;
+
+    setTimeout(() => {
+      if (i > 100) {
+        const buf = Buffer.from(String(i));
+
+        this.push(buf);
+      }
+    }, 1000);
+  }
 }
+
+class MultiplyByTenStream extends Writable {
+  index = i;
+
+  _;
+}
+
+new OneToHundredStream().pipe(process.stdout);
